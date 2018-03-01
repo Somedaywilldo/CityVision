26842a966a5f975622d570d4f209b8cc3796f1f6
Somedaywilldo <714439471@qq.com>
2018-03-01 23:21:52 +0800

    test

--- Semantic_Segmentation/perform_segmentation.py
+++ Semantic_Segmentation/perform_segmentation.py
@@ -4,7 +4,7 @@ import threading
 from time import ctime,sleep
 
 def segmentation(x):
-	os.system("python test_segmentation.py --city_name "+x)
+	os.system("python Baidu_segmentation.py --city_name "+x)
 
 threads = []
 t1 = threading.Thread(target=segmentation,args=(u"Beijing",))
@@ -15,7 +15,6 @@ t3 = threading.Thread(target=segmentation,args=(u"Guangzhou",))
 threads.append(t3)
 
 if __name__ == '__main__':
-	#多线程，一个city一个线程
     for t in threads:
         t.setDaemon(True)
         t.start()
